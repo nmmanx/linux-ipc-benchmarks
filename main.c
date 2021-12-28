fdac642700fd1f62e69b975cbbb32a70c126dcce
Man Nguyen <nmman133@gmail.com>
2021-12-28 23:32:19 +0700

    Added for_each_ipc to find all implemented ipc facilities

--- main.c
+++ main.c
@@ -6,8 +6,28 @@
 extern void *__ipc_array_start;
 extern void *__ipc_array_end;
 
+void for_each_ipc(int(*cb)(ipc_t*))
+{
+    static const int align = 32;
+    int n = ((sizeof(ipc_t) + align) - (sizeof(ipc_t) % align)) / sizeof(void*);
+    int i = 0;
+    void *p = &__ipc_array_start;
+
+    do {
+        p = &__ipc_array_start + (i++)*n;
+    } while (p < &__ipc_array_end && !cb(p));
+}
+
+int print_name(ipc_t *ipc)
+{
+    printf("Found IPC facility: %s\n", ipc->name);
+    return 0;
+}
+
 int main()
 {
+    for_each_ipc(print_name);
+
     ipc_t *ipc = (ipc_t*)&__ipc_array_start;
 
     report_t rp;
